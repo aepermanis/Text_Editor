0b270141de464fbbd55eccb58bce31a0ec65d2ba
[Andrew Epermanis] <[aepermanis@gmail.com]>
2023-11-12 15:54:58 -0500

    added functionality to application to add to the database and retrieve all database items

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,10 +12,25 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB('jate', 1);
+  const permission = jateDb.transaction('jate', 'readwrite');
+  const storage = permission.objectStore('jate');
+  const request = storage.add({ content });
+  const result = await request;
+  console.log('Data saved to the database', result);
+};;
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+
+export const getDb = async () => {
+  const jateDb = await openDB('jate', 1);
+  const permission = jateDb.transaction('jate', 'readonly');
+  const storage = permission.objectStore('jate');
+  const request = storage.getAll();
+  const result = await request;
+  console.log('result.value', result);
+  return result;
+};
+
 
 initdb();
